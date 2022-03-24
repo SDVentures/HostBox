77ae07e92f90d75f502fb93971dbcfe6dbb09965
Vitaly Pomykalov <v.pomykalov@sdventures.com>
2022-03-24 12:17:49 +0400

    Overriding and prioritization configurations in shared and  service

--- Sources/HostBox/ConfigFileNamesProvider.cs
+++ Sources/HostBox/ConfigFileNamesProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace HostBox
 {
@@ -8,20 +9,27 @@ namespace HostBox
     {
         private const string SettingsPath = "settings";
 
+        private const string SettingsOverridingPath = "settingsOverriding";
+
         private const string ValuesPath = "values";
 
         private const string ConfigFilePattern = "*.settings.json";
 
-        private const string TemplateValuesFile = "values.json";
+        private readonly Regex prioritySettingRegex = new Regex(@"^(?<settingName>\w+)\.(?<priority>\d+)\.settings\.json$");
+
+        private readonly Regex settingNameRegex = new Regex(@"^(?<settingName>\w+)(\.(?<priority>\d+))?\.settings\.json$");
 
         private readonly string configName;
 
         private readonly string basePath;
 
-        public ConfigFileNamesProvider(string configName, string basePath)
+        private readonly string settingOverridingFullPath;
+
+        public ConfigFileNamesProvider(string configName, string basePath, string sharedLibraryPath)
         {
             this.configName = configName;
             this.basePath = basePath;
+            this.settingOverridingFullPath = Path.Combine(sharedLibraryPath, SettingsOverridingPath);
         }
 
         public IEnumerable<string> GetTemplateValuesFiles()
@@ -41,22 +49,63 @@ namespace HostBox
 
         public IEnumerable<string> EnumerateConfigFiles()
         {
-            var pathsToSearchIn = new[] { SettingsPath }.Union(this.EnumerateEnvSubfolders());
+            IEnumerable<string> result = null;
 
-            foreach (var path in pathsToSearchIn)
+            var configFiles = this.GetFilesWithPath(SettingsPath);
+            var overridingConfigFiles = this.GetFilesWithPath(this.settingOverridingFullPath);
+
+            var configFilesMatches = configFiles.Select(x => x.Key)
+                                                .Select(x => this.prioritySettingRegex.Match(x))
+                                                .Where(m => m.Success)
+                                                .ToList();
+
+            if (configFilesMatches.Any())
             {
-                var fullPath = Path.Combine(this.basePath, path);
+                var overridingResult = new List<string>();
 
-                if (!Directory.Exists(fullPath))
+                foreach (var configFileMatch in configFilesMatches)
                 {
-                    continue;
+                    var settingName = configFileMatch.Groups["settingName"].Value;
+                    var configPriority = int.Parse(configFileMatch.Groups["priority"].Value);
+                    var configFilePath = configFiles[configFileMatch.Value];
+
+                    foreach (var (overridingFileName, overridingFilePath) in overridingConfigFiles)
+                    {
+                        var match = this.settingNameRegex.Match(overridingFileName);
+
+                        if (!match.Success || settingName != match.Groups["settingName"].Value)
+                        {
+                            continue;
+                        }
+
+                        var matchPriority = match.Groups["priority"];
+                        var overridingConfigPriority = matchPriority.Success ? int.Parse(matchPriority.Value) : 1;
+
+                        if (configPriority > overridingConfigPriority)
+                        {
+                            overridingResult.Add(overridingFilePath);
+                            overridingResult.Add(configFilePath);
+                        }
+                        else
+                        {
+                            overridingResult.Add(configFilePath);
+                            overridingResult.Add(overridingFilePath);
+                        }
+
+                        break;
+                    }
                 }
 
-                foreach (var file in Directory.GetFiles(fullPath, ConfigFilePattern, SearchOption.TopDirectoryOnly))
-                {
-                    yield return file;
-                }
+                result = overridingResult.Union(configFiles.Select(x => x.Value))
+                                         .Union(overridingConfigFiles.Select(x => x.Value))
+                                         .Distinct();
             }
+            else
+            {
+                result = configFiles.Select(x => x.Value).Union(overridingConfigFiles.Select(x => x.Value));
+            }
+
+            return result.Union(this.EnumerateEnvSubfolders().SelectMany(this.EnumerateFiles));
         }
 
         private IEnumerable<string> EnumerateEnvSubfolders()
@@ -67,6 +116,26 @@ namespace HostBox
             }
 
             yield return Path.Combine(SettingsPath, this.configName.ToLower());
+        }
+
+        private IEnumerable<string> EnumerateFiles(string path)
+        {
+            var fullPath = Path.Combine(this.basePath, path);
+
+            if (!Directory.Exists(fullPath))
+            {
+                yield break;
+            }
+
+            foreach (var file in Directory.GetFiles(fullPath, ConfigFilePattern, SearchOption.TopDirectoryOnly))
+            {
+                yield return file;
+            }
+        }
+
+        private Dictionary<string, string> GetFilesWithPath(string path)
+        {
+            return this.EnumerateFiles(path).ToDictionary(Path.GetFileName, p => p);
         }
     }
 }
