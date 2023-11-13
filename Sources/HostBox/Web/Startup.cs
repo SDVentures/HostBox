fb3496fa407bcc4aebfa11a3e5827f5b55bee911
d.krasakov <d.krasakov@sdventures.com>
2023-11-13 10:55:56 +0200

    return 3.1 support

--- Sources/HostBox/Web/Startup.cs
+++ Sources/HostBox/Web/Startup.cs
@@ -11,10 +11,14 @@ namespace HostBox.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // Calling 'BuildServiceProvider' from application code results in more than one copy of
+            // singleton services being created which might result in incorrect application behavior.
+            // Consider alternatives such as dependency injecting services as parameters to 'Configure'.
             var sp = services.BuildServiceProvider();
             var startup = sp.GetService<IStartup>();
 
             startup?.ConfigureServices(services);
+            
             HostboxWebExtensions.AddHealthChecks(services);
         }
 
